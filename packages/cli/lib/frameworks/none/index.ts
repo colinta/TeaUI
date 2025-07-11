7a9ffe71b1b3403f53274e0dac991b00b7bc5c5b
Colin T.A. Gray <colinta@gmail.com>
2025-07-11 08:17:30 -0400

    first stab at a cli: 'teaui create --framework none|react|preact'

---
+++ packages/cli/lib/frameworks/none/index.ts
@@ -0,0 +1,57 @@
+import {
+  Screen,
+  TrackMouse,
+  ConsoleLog,
+  Button,
+  Stack,
+  Separator,
+  Space,
+  Text,
+  Window,
+  interceptConsoleLog,
+} from '@teaui/core'
+;(async () => {
+  interceptConsoleLog()
+  console.log('Logs appear in the "ConsoleLog" component')
+  setTimeout(() => {
+    console.log('one more log for good measure')
+  }, 1000)
+
+  process.title = '{{name}}'
+  const consoleLog = new ConsoleLog({
+    height: 10,
+  })
+  const [screen, program] = await Screen.start(
+    new Window({
+      child: new TrackMouse({
+        content: Stack.down([
+          [
+            'flex1',
+            Stack.down([
+              new Text({alignment: 'center', text: 'Hello to "{{name}}"'}),
+              new Separator({
+                direction: 'horizontal',
+                border: 'trailing',
+                padding: 1,
+              }),
+              Space.vertical(10),
+              new Button({
+                title: 'Exit',
+                onClick: () => {
+                  screen.exit()
+                },
+              }),
+            ]),
+          ],
+          ['natural', consoleLog],
+        ]),
+      }),
+    }),
+    {quitChar: 'q'},
+  )
+
+  program.key('escape', function () {
+    consoleLog.clear()
+    screen.render()
+  })
+})()
